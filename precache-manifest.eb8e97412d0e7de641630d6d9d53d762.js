f17a42eaa4cafbab947fa82d12883f8676422bc4
Aakarsh <tbeflash@gmail.com>
2020-10-04 22:46:13 +0530

    Updates

---
+++ precache-manifest.eb8e97412d0e7de641630d6d9d53d762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700ce7033b25a0e516a3fe9999f77d40",
+    "url": "/Quiz-Application/index.html"
+  },
+  {
+    "revision": "98ae5339a3f49812044d",
+    "url": "/Quiz-Application/static/css/main.4af7c47e.chunk.css"
+  },
+  {
+    "revision": "29d4fe8abd413cb2cfcd",
+    "url": "/Quiz-Application/static/js/2.9f67909d.chunk.js"
+  },
+  {
+    "revision": "2ad22aa07678110d0211169ae951cc28",
+    "url": "/Quiz-Application/static/js/2.9f67909d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ae5339a3f49812044d",
+    "url": "/Quiz-Application/static/js/main.98ef6750.chunk.js"
+  },
+  {
+    "revision": "b8b3758602f93a1e714f",
+    "url": "/Quiz-Application/static/js/runtime-main.053b658b.js"
+  }
+]);
